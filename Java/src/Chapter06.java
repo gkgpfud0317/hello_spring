a609deb710ed85b2b61ced416be582b040ca0c52
하혜령 <gkgpfud1004@gmail.com>
2022-01-05 15:41:00 +0900

    [ADD] Java_Chapter06

---
+++ Java/src/Chapter06.java
@@ -0,0 +1,13 @@
+public class Chapter06 {
+    public static void main(String[] args) {
+
+        byte bnum = 127;
+
+        System.out.println(bnum);
+
+        //int num = 12345678900;
+        long lNum = 12345678900L;
+
+        System.out.println(lNum);
+    }
+}

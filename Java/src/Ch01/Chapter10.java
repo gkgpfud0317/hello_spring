31472cb39f6e287cdaa4ab593d7747c6a1fb3f24
하혜령 <gkgpfud1004@gmail.com>
2022-01-06 01:52:32 +0900

    [ADD] Charter10

---
+++ Java/src/Ch01/Chapter10.java
@@ -0,0 +1,16 @@
+package Ch01;
+
+public class Chapter10 {
+    public static void main(String[] args) {
+
+        final int MAX_NUM = 100;
+        final int MIN_NUM;
+
+        MIN_NUM = 0;
+        //MAX_NUM = 20;
+
+        System.out.println(MAX_NUM);
+        System.out.println(MIN_NUM);
+
+    }
+}

8ae9727e1afd567a467e80f77cd1d23b57a86566
하혜령 <gkgpfud1004@gmail.com>
2022-01-05 15:42:44 +0900

    [FIX] MemberService

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -24,7 +24,7 @@ public class MemberService {
         return member.getId();
     }
 
-    private void validateDuplicateMember(@org.jetbrains.annotations.NotNull Member member) {
+    private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName()).ifPresent(m -> {
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });

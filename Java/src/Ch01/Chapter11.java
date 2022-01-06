db1c2a865e17e090383b9d4c2f84b7cdf18f9f90
하혜령 <gkgpfud1004@gmail.com>
2022-01-07 00:01:35 +0900

    [ADD] Chapter11

---
+++ Java/src/Ch01/Chapter11.java
@@ -0,0 +1,13 @@
+package Ch01;
+
+public class Chapter11 {
+    public static void main(String[] args) {
+
+        int gameScore = 150;
+
+        int lastScore = gameScore--; // gameScore += 1; gameScore +1;
+
+        System.out.println(lastScore);
+        System.out.println(gameScore);
+    }
+}

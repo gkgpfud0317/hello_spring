55e87840b0bb728b841a85b82d1f715aa69f204a
하혜령 <gkgpfud1004@gmail.com>
2022-01-07 00:01:41 +0900

    [ADD] Chapter12

---
+++ Java/src/Ch01/Chapter12.java
@@ -0,0 +1,23 @@
+package Ch01;
+
+public class Chapter12 {
+    public static void main(String[] args) {
+
+        int num1 = 10;
+        int i = 2;
+
+        /*boolean value = ( (num1 = num1 + 10) < 10) && ( (i = i + 2 ) < 10);
+        System.out.println(value);
+        System.out.println(num1);
+        System.out.println(i); // 뒤쪽항울 계산하지 않음
+        */
+
+        /*boolean value = ( (num1 = num1 + 10) < 10) || ( (i = i + 2 ) < 10);
+        System.out.println(value);
+        System.out.println(num1);
+        System.out.println(i);
+         */
+    }
+
+}
+

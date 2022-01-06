9a243908b5e9c1a3a6ca2b231bf69fd760f68961
하혜령 <gkgpfud1004@gmail.com>
2022-01-07 01:34:03 +0900

    [ADD] Chapter19

---
+++ Java/src/Ch01/Chapter19.java
@@ -0,0 +1,25 @@
+package Ch01;
+
+public class Chapter19 {
+    public static void main(String[] args) {
+
+        int count = 1;
+        int sum = 0;
+
+        for (int i = 0; i < 10; i++, count++) {
+
+            sum += count;
+        }
+        System.out.println(sum);
+
+        int num = 1;
+        int total = 0;
+
+        while ( num <= 10 ) {
+            total += num;
+            num++;
+        }
+        System.out.println(total);
+    }
+}
+

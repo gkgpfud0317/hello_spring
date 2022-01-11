680c54a52358e68ff368f6c4e23079609980521c
하혜령 <gkgpfud1004@gmail.com>
2022-01-12 04:37:50 +0900

    [ADD] BoardService

---
+++ CRUD/src/main/java/com/example/crud/service/BoardService.java
@@ -0,0 +1,21 @@
+package com.example.crud.service;
+
+import com.example.crud.dto.BoardRequest;
+import com.example.crud.entity.Board;
+import com.example.crud.entity.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor // 의존성 주입
+public class BoardService {
+
+    private final BoardRepository repository;
+
+    public void create(BoardRequest request) {
+        Board board = Board.builder()
+                .title(request.getTitle())
+                .build();
+        repository.save(board);
+    }
+}

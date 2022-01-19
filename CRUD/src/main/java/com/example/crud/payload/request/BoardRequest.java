edfc08c634ece333ca6b286c6c161a5a0d22cfcd
하혜령 <gkgpfud1004@gmail.com>
2022-01-20 00:48:32 +0900

    [UPDATE] BoardRequest

--- CRUD/src/main/java/com/example/crud/payload/request/BoardRequest.java
+++ CRUD/src/main/java/com/example/crud/payload/request/BoardRequest.java
@@ -1,17 +1,13 @@
-package com.example.crud.dto;
+package com.example.crud.payload.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
-@NoArgsConstructor // request에 씀
 @Builder
+@AllArgsConstructor
 public class BoardRequest {
-
     private String title;
-
     private String content;
 }

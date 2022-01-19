397ac2daeadf40bcdcdbc48a74da492e9001acbd
하혜령 <gkgpfud1004@gmail.com>
2022-01-20 00:48:09 +0900

    [UPDATE] BoardController

--- CRUD/src/main/java/com/example/crud/controller/BoardController.java
+++ CRUD/src/main/java/com/example/crud/controller/BoardController.java
@@ -1,18 +1,26 @@
 package com.example.crud.controller;
 
-import com.example.crud.dto.BoardRequest;
+import com.example.crud.entity.Board;
+import com.example.crud.payload.request.BoardRequest;
 import com.example.crud.service.BoardService;
+import com.example.crud.service.BoardServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequiredArgsConstructor // 의존성 주입
+@RequiredArgsConstructor
 public class BoardController {
 
-    private final BoardService service;
+    private final BoardServiceImpl service;
 
-    @PostMapping("/create")
+    // 작성
+    @PostMapping("/write")
     public void create(@RequestBody BoardRequest request) {
         service.create(request);
+    }
+    // 삭제
+    @DeleteMapping("/delete")
+    public void delete(@PathVariable Integer id) {
+        service.delete(id);
     }
 }

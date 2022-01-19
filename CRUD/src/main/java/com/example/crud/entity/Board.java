f60f9b1abb0422a3762762062ccdf260602a9151
하혜령 <gkgpfud1004@gmail.com>
2022-01-20 00:47:55 +0900

    [ADD] BoardEntity

--- CRUD/src/main/java/com/example/crud/entity/Board.java
+++ CRUD/src/main/java/com/example/crud/entity/Board.java
@@ -11,10 +11,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-@NoArgsConstructor //  파라미터가 없는 기본 생성자를 생성
-@AllArgsConstructor //  모든 필드 값을 파라미터로 받는 생성자를 만듦
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
-@Builder // 자동으로 해당 클래스에 빌더를 추가
+@Builder
 public class Board {
 
     @Id

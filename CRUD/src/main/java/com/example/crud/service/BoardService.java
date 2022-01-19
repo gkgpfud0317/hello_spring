83b3ff4b57da407249bee273e9bed401db7d65d4
하혜령 <gkgpfud1004@gmail.com>
2022-01-20 00:47:49 +0900

    [ADD] BoardService

--- CRUD/src/main/java/com/example/crud/service/BoardService.java
+++ CRUD/src/main/java/com/example/crud/service/BoardService.java
@@ -1,23 +1,8 @@
 package com.example.crud.service;
 
-import com.example.crud.dto.BoardRequest;
-import com.example.crud.entity.Board;
-import com.example.crud.entity.BoardRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+import com.example.crud.payload.request.BoardRequest;
 
-@Service
-@RequiredArgsConstructor // 의존성 주입
-public class BoardService {
-
-    private final BoardRepository repository;
-
-    public void create(BoardRequest request) {
-        Board board = Board.builder()
-                .title(request.getTitle())
-                .content(request.getContent())
-                .build();
-
-        repository.save(board);
-    }
+public interface BoardService {
+    void create(BoardRequest request);
+    void delete(Integer id);
 }

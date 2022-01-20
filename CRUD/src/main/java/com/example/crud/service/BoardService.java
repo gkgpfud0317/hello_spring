44af463e743896ffde5c4c7f645aafc171296a06
하혜령 <gkgpfud1004@gmail.com>
2022-01-21 00:21:07 +0900

    [UPDATE] BoardService

--- CRUD/src/main/java/com/example/crud/service/BoardService.java
+++ CRUD/src/main/java/com/example/crud/service/BoardService.java
@@ -1,8 +1,24 @@
 package com.example.crud.service;
 
 import com.example.crud.payload.request.BoardRequest;
+import com.example.crud.payload.response.BoardResponse;
+
+import java.util.List;
 
 public interface BoardService {
+
+    // 작성
     void create(BoardRequest request);
+
+    // 삭제
     void delete(Integer id);
+
+    // 수정
+    void update(Integer id, BoardRequest request);
+
+    // GET
+    BoardResponse read(Integer id);
+
+    // 리스트
+    List<BoardResponse> list(int userId);
 }

db169ac4386a2c21a699d4adeb5f7d25a379a648
하혜령 <gkgpfud1004@gmail.com>
2022-01-21 00:21:39 +0900

    [UPDATE] BoardRepository

--- CRUD/src/main/java/com/example/crud/entity/BoardRepository.java
+++ CRUD/src/main/java/com/example/crud/entity/BoardRepository.java
@@ -2,6 +2,8 @@ package com.example.crud.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BoardRepository extends JpaRepository<Board, Integer> {
+import java.util.List;
 
+public interface BoardRepository extends JpaRepository<Board, Integer> {
+    List<Board> findByUserId(int userId);
 }

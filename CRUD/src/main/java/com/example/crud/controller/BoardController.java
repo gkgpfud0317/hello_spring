82debab024268f701331f752c262354f482ecffb
하혜령 <gkgpfud1004@gmail.com>
2022-01-21 00:21:21 +0900

    [UPDATE] BoardController

--- CRUD/src/main/java/com/example/crud/controller/BoardController.java
+++ CRUD/src/main/java/com/example/crud/controller/BoardController.java
@@ -1,11 +1,13 @@
 package com.example.crud.controller;
 
-import com.example.crud.entity.Board;
 import com.example.crud.payload.request.BoardRequest;
-import com.example.crud.service.BoardService;
+import com.example.crud.payload.response.BoardResponse;
 import com.example.crud.service.BoardServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,12 +17,31 @@ public class BoardController {
 
     // 작성
     @PostMapping("/write")
-    public void create(@RequestBody BoardRequest request) {
+    public void create(@Valid @RequestBody BoardRequest request) {
         service.create(request);
     }
     // 삭제
     @DeleteMapping("/delete")
-    public void delete(@PathVariable Integer id) {
+    public void delete(@Valid @PathVariable Integer id) {
         service.delete(id);
     }
+
+    // 수정
+    @PutMapping("/update")
+    public void update(@Valid @PathVariable Integer id, @RequestBody BoardRequest request) {
+        service.update(id, request);
+    }
+
+    // GET
+    @GetMapping("/read")
+    public BoardResponse read(@PathVariable Integer id) {
+        return service.read(id);
+    }
+
+    // 리스트
+    @GetMapping("/list")
+    public List<BoardResponse> list(@PathVariable int userId) {
+        return service.list(userId);
+    }
+
 }

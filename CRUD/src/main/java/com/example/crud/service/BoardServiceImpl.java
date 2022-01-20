45af80c2b5668f60dcad1dd1815b68134ffbc445
하혜령 <gkgpfud1004@gmail.com>
2022-01-21 00:20:48 +0900

    [UPDATE] BoardServiceImpl

--- CRUD/src/main/java/com/example/crud/service/BoardServiceImpl.java
+++ CRUD/src/main/java/com/example/crud/service/BoardServiceImpl.java
@@ -3,9 +3,11 @@ package com.example.crud.service;
 import com.example.crud.entity.Board;
 import com.example.crud.entity.BoardRepository;
 import com.example.crud.payload.request.BoardRequest;
+import com.example.crud.payload.response.BoardResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -26,5 +28,31 @@ public class BoardServiceImpl implements BoardService {
     @Override
     public void delete(Integer id) {
         repository.deleteById(id);
+    }
+
+    // 수정
+    @Override
+    public void update(Integer id, BoardRequest request) {
+        Board board = repository.findById(id)
+                .map(newBoard -> newBoard.updateBoard(
+                        request.getTitle(),
+                        request.getContent()
+                ))
+                .orElseThrow(() -> new RuntimeException());
+    }
+
+    // GET
+    @Override
+    public BoardResponse read(Integer id) {
+        return repository.findById(id)
+                .map(Board -> {
+                    BoardResponse response = BoardResponse.builder()
+                            .id(Board.getId())
+                            .title(Board.getTitle())
+                            .content(Board.getContent())
+                            .build();
+                    return response;
+                })
+                .orElseThrow(() -> new RuntimeException());
     }
 }

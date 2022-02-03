512cd96f7a06b7c0d6cbda6f7c4bce9c249eb992
하혜령 <gkgpfud1004@gmail.com>
2022-02-04 02:28:36 +0900

    [ADD] 비밀번호 중복 확인 Repository

--- SignUp/src/main/java/com/example/signup/entity/SignRepository.java
+++ SignUp/src/main/java/com/example/signup/entity/SignRepository.java
@@ -2,8 +2,11 @@ package com.example.signup.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SignRepository extends JpaRepository<Long, SignEntity> {
+public interface SignRepository extends JpaRepository<SignEntity, Long> {
 
     // 아이디 중복 확인
     boolean existsByuserId(String userId);
+
+    // 비밀번호 중복 확인
+    boolean existsByPassword(String password);
 }

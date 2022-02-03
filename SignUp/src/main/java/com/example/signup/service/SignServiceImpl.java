b4082c5f81da03f956b4e0351181e1ab1ab674b6
하혜령 <gkgpfud1004@gmail.com>
2022-02-04 02:28:19 +0900

    [ADD] 비밀번호 중복 확인 ServiceImpl

--- SignUp/src/main/java/com/example/signup/service/SignServiceImpl.java
+++ SignUp/src/main/java/com/example/signup/service/SignServiceImpl.java
@@ -1,11 +1,16 @@
 package com.example.signup.service;
 
+import com.example.signup.dto.SignDto;
+import com.example.signup.entity.SignEntity;
 import com.example.signup.entity.SignRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+// Rollback 할 수 있도록 안전장치
+@Transactional(readOnly = true)
 public class SignServiceImpl implements SignService {
 
     private final SignRepository repository;
@@ -13,6 +18,11 @@ public class SignServiceImpl implements SignService {
     // 아이디 중복 확인
     public boolean checkuserIdDuplicate(String userId) {
         return repository.existsByuserId(userId);
+    }
+
+    // 비밀번호 중복 확인
+    public boolean checkPasswordDuplicate(String password) {
+        return repository.existsByPassword(password);
     }
 
 }

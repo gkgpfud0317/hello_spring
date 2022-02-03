3908e673e57b509bf0c0e1e4ec6a5241a558a460
하혜령 <gkgpfud1004@gmail.com>
2022-02-04 02:28:56 +0900

    [ADD] 비밀번호 암호화 Dto

--- SignUp/src/main/java/com/example/signup/dto/SignDto.java
+++ SignUp/src/main/java/com/example/signup/dto/SignDto.java
@@ -1,12 +1,21 @@
 package com.example.signup.dto;
 
-import com.sun.istack.NotNull;
+import com.example.signup.entity.SignEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class SignDto {
+
+    private Long id;
 
     @NotBlank(message = "아이디를 입력해주세요.")
     @Size(min = 5, max = 10, message = "아이디는 5자 이상 10자 이하로 입력해주세요.")
@@ -30,4 +39,17 @@ public class SignDto {
     @NotBlank(message = "성별을 선택하세요.")
     private String gender;
 
+    // 비밀번호 암호화
+    public SignEntity toEntity() {
+        return SignEntity.builder()
+                .id(id)
+                .userId(userId)
+                .email(email)
+                .username(username)
+                .password(new BCryptPasswordEncoder().encode(password))
+                .birth(birth)
+                .gender(gender)
+                .build();
+    }
 }
+
